c90c71c141083ecbbd10e150f64a53f20d0fecd8
DerekLoveCC <zhaomingbo1010@126.com>
2020-05-12 15:38:06 +0800

    add IList Extension metods

---
+++ CSharpExtensionMehods/IListExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+
+namespace CSharpExtensionMehods
+{
+    public static class IListExtensions
+    {
+        public static T FirstOrDeafult<T>(this IList list, Predicate<T> predicate)
+        {
+            foreach (var listItem in list)
+            {
+                if (listItem is T li && predicate(li))
+                {
+                    return li;
+                }
+            }
+
+            return default(T);
+        }
+    }
+}
