45960adfb6ab7e83c89b284145c0d2da7ee8d1be
Derek <derek@smallboat.com>
2024-01-17 18:26:33 +0800

    upgrade to .net 8

--- CSharpExtensionMehods/Properties/AssemblyInfo.cs
+++ CSharpExtensionMehods/Properties/AssemblyInfo.cs
@@ -1,16 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("CSharpExtensionMethods")]
-[assembly: AssemblyDescription("Some useful C# extension methods")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Derek Zhao")]
-[assembly: AssemblyProduct("CSharpExtensionMethods")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +11,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("7b6d7df1-53d3-4ee5-a018-6aa41ec37588")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.1.0")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
